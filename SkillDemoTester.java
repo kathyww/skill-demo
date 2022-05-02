64d0dd3212d2f7a285af0b15c605104a4179b91f
kathyww <kes.wen404@gmail.com>
2022-05-01 23:46:26 -0700

    update code( tester )

---
+++ SkillDemoTester.java
@@ -0,0 +1,12 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class SkillDemoTester {
+    @Test
+    public void minusTester() {
+        assertEquals(3, SkillDemo.minus(5, 4));
+    }
+    
+}
